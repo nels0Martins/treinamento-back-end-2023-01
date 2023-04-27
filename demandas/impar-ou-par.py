80c296118fb31df7ef26727dae9674790d1a2e67
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2023-04-27 16:50:53 -0300

    enviando mais duas demandas

---
+++ demandas/impar-ou-par.py
@@ -0,0 +1,6 @@
+numero = float(input("Digite um número inteiro: "))
+
+if numero % 2 == 0:
+    print("O número {:.0f} é PAR!".format(numero))
+else:
+    print("O número {:.0f} é IMPAR!".format(numero))
---
+++ demandas/sorteio.py
@@ -0,0 +1,12 @@
+import random 
+
+print("SORTEANDO ENTRE 3 PESSOAS")
+
+nome1 = input("1° Digite seu nome: ")
+nome2 = input("2° Digite seu nome: ")
+nome3 = input("3° Digite seu nome: ")
+
+lista = [nome1, nome2, nome3]
+sorteio = random.choice(lista)
+
+print("A pessoa sorteada foi: {}".format(sorteio))
